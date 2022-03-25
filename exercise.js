c40db074fcfa912f3e21e36ee2494ef7da0260d9
Jon Lake <jlake21@ford.com>
2022-03-25 12:52:10 -0600

    Add functions

--- exercise.js
+++ exercise.js
@@ -2,9 +2,27 @@ function getTotalItemSales(sales, id){
     return sales.filter(sale => sale.itemId === id).map(sale => sale.quantity).reduce((a, b) => a + b, 0) 
 }
 
+function addTotalSalesToItems(sales, items){
+    return items.map(item =>{
+        return {
+            ...item,
+            quantity: getTotalItemSales(sales, item.id)
+        }
+    })
+}
+
+function addTotalValueToItems(sales, items) {
+    return addTotalSalesToItems(sales, items).map(item => {
+        item.totalValue = (item.quantity * item.price).toFixed(2)
+        return item;
+    })
+}
+
+
+
 module.exports = {
     // Uncomment these functions as you write them
     getTotalItemSales,
-    //addTotalSalesToItems,
-    //addTotalValueToItems
+    addTotalSalesToItems,
+    addTotalValueToItems
 }
