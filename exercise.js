3f822e96009f5244a1f7e99eca8a937cd4a1abdb
bcherry4 <bcherry4@ford.com>
2022-03-25 10:59:10 -0600

    Add get item sales

--- exercise.js
+++ exercise.js
@@ -1,6 +1,10 @@
+function getTotalItemSales(sales, id){
+    return sales.filter(sale => sale.itemId === id).map(sale => sale.quantity).reduce((a, b) => a + b, 0) 
+}
+
 module.exports = {
     // Uncomment these functions as you write them
     getTotalItemSales,
-    addTotalSalesToItems,
-    addTotalValueToItems
+    //addTotalSalesToItems,
+    //addTotalValueToItems
 }
